e0a220263cb3fe77ca99eb9d2619fb3b39dd2095
Alexandros Zoupas <alexandros.p.zoupas@gmail.com>
2022-08-04 21:21:09 +0300

    something

--- app/controllers/postings_controller.rb
+++ app/controllers/postings_controller.rb
@@ -2,4 +2,29 @@ class PostingsController < ApplicationController
   def index
     @postings = policy_scope(Posting)
   end
+
+  def new
+    @posting = Posting.new
+  end
+
+  def create
+    @posting = Posting.new(posting_params)
+    @user = User.find(params[:user_id])
+    @review.user = @user
+    if @posting.save
+      redirect_to user_path(@user)
+    else
+      render 'new'
+    end
+  end
+
+  def find_user
+    @user = User.find(params[:user_id])
+  end
+
+  private
+
+  def posting_params
+    params.require(:posting).permit(:cuisine, :price, :bio)
+  end
 end
